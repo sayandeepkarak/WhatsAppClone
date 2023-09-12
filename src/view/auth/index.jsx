f47d35d5d867a57110b5ab61cdc8b3a75d27feb9
Sayandeep Karak <sayandeep18.webdev@gmail.com>
2023-09-12 18:09:19 +0530

    bug on the authentication route was fixed

--- src/view/auth/index.jsx
+++ src/view/auth/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   AuthLogoBlock,
   AuthScreenBlock,
@@ -7,19 +7,39 @@ import {
 } from "./auth.styled";
 import logo from "../../assets/images/icon-transparent.png";
 import AuthBox from "./AuthBox";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import LoaderScreen from "../../components/Loader";
 
 const Auth = () => {
+  const navigate = useNavigate();
+  const [isLoadable, setIsLoadable] = useState(true);
+
+  useEffect(() => {
+    const refreshtoken = Cookies.get("refresh-key");
+    if (refreshtoken) {
+      navigate("/");
+    } else {
+      setTimeout(() => {
+        setIsLoadable((old) => !old);
+      }, 400);
+    }
+  }, [navigate]);
   return (
     <>
-      <AuthScreenBlock>
-        <AuthScreenTop>
-          <AuthLogoBlock>
-            <Logo src={logo} />
-            <span>whatsapp clone</span>
-          </AuthLogoBlock>
-        </AuthScreenTop>
-        <AuthBox />
-      </AuthScreenBlock>
+      {isLoadable ? (
+        <LoaderScreen />
+      ) : (
+        <AuthScreenBlock>
+          <AuthScreenTop>
+            <AuthLogoBlock>
+              <Logo src={logo} />
+              <span>whatsapp clone</span>
+            </AuthLogoBlock>
+          </AuthScreenTop>
+          <AuthBox />
+        </AuthScreenBlock>
+      )}
     </>
   );
 };
